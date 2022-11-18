c264d3132da765f2be59086cfaf80ca685b4a7ef
Salavat <salavatmerhatimovic@gmail.com>
2022-11-19 00:09:24 +0300

    feat: финальная дороботка интерактив футер-меню,бургер-меню

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# RaikinPlazaMob
-RaikinPlazaMob
+# Проект : RaikinPlazaMob
+
+### Описание
+* Одностраничный сайт,выполнен в качестве тестового задания.
+ 
+### Стек
+* html
+* sass
+* bem
+* js
+
+### Запуск проекта
+* npm run build
+* npm run start
+
+
+
+*ссылка на сайт*
+<!-- (https://salavat698.github.io/russian-travel/) -->
--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import './index.html'
 import './index.sass'
-console.log('!')
-console.log('!')
-console.log('!')
+const nav = document.querySelectorAll('.nav')
+const btnBugerOpen = document.querySelector('.header__hamburger-menu')
+const btnBugerClose = document.querySelector('.buger-menu__close')
+const bugerMenu = document.querySelector('.buger-menu')
+
+// управление активности в icon-lists элментами
+nav.forEach(item=>{
+    item.addEventListener( 'click' ,()=>{
+            nav.forEach(item=>{
+                item.classList.remove('active')
+            })
+            item.classList.add('active')   
+    })  
+})
+// управление открыть/закрыть бургер меню
+btnBugerOpen.addEventListener('click',()=>{
+    bugerMenu.classList.add('active-burger')
+})
+btnBugerClose.addEventListener('click',()=>{
+    bugerMenu.classList.remove('active-burger')
+})
